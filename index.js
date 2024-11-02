6aa3a6cc798b6cd81859332675d7977e4a04da66
evelisedias <evelisemacieldias@outlook.com>
2024-11-02 19:15:36 -0300

    atualizando portfolio

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-$(function(){
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 60)
-        {
-            $('#navbar').addClass('navbar-fixed-top', 500);
+$(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 60) {
+            $('#navbar').addClass('navbar-fixed-top');
         } else {
-            $('#navbar').removeClass('navbar-fixed-top', 500);  
+            $('#navbar').removeClass('navbar-fixed-top');
         }
     });
+});
+
+
+
+document.getElementById("music").addEventListener("click", function() {
+    if (this.paused) {
+        this.play();
+    } else {
+        this.pause();
+    }
 });
