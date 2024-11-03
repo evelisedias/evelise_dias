0ebfa583348dae8d2841b4df66e8541cbd6e5e03
evelisedias <evelisemacieldias@outlook.com>
2024-11-02 23:11:28 -0300

    incluindo responsividade

--- index.js
+++ index.js
@@ -17,3 +17,21 @@ document.getElementById("music").addEventListener("click", function() {
         this.pause();
     }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    function updateButtonText() {
+        const downloadBtn = document.getElementById("downloadBtn");
+
+        if (window.innerWidth <= 600) {
+            downloadBtn.textContent = "Ver currículo";
+        } else {
+            downloadBtn.textContent = "Clique aqui para acessar o meu currículo";
+        }
+    }
+
+    // Executa a função no carregamento da página
+    updateButtonText();
+
+    // Monitora mudanças no tamanho da tela para atualizar o texto dinamicamente
+    window.addEventListener("resize", updateButtonText);
+});
